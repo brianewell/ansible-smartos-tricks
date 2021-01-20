fffceb37e98d7475535fdfabdcbdf534c2a7e421
Brian Ewell <brian@brianewell.com>
2021-01-20 02:34:59 +0000

    Added additional ansible prerequisites through pkgin instead of pip Fixed reference to bootstrap.yml in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # For now, install py39-pip and core ansible dependencies
-/opt/local/bin/pkgin -y install py38-pip py38-cryptography
+/opt/local/bin/pkgin -y install py38-cryptography py38-jinja2 py38-pip py38-pyparsing
 
 # Install ansible directly via pip
 /opt/local/bin/pip install ansible
 
 # Use Ansible to update ansible
-/opt/local/bin/ansible-playbook ansible-bootstrap.yml
+/opt/local/bin/ansible-playbook bootstrap.yml
